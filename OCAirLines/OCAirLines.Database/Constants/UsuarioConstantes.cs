7028409447f5aa10993ef0e1e3f0e2aa66bb271e
Gleydson Morais <morais_100@hotmail.com>
2020-10-12 19:37:50 -0300

    criação dos objetos e contexto (mapeamento dos obejos Usuario e Cartao)

---
+++ OCAirLines/OCAirLines.Database/Constants/UsuarioConstantes.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OCAirLines.Database.Constants
+{
+    public class UsuarioConstantes
+    {
+        /// <summary>
+        /// CPF
+        /// </summary>
+        public static readonly string CPF = "CPF";
+
+        /// <summary>
+        /// RG
+        /// </summary>
+        public static readonly string RG = "RG";
+
+        /// <summary>
+        /// Passaporte
+        /// </summary>
+        public static readonly string Passaporte = "Passaporte";
+    }
+}
